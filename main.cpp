4d4785468fbdac76cd4fc52564f2923fcc84415a
Alice (SonAE) <alice@example.com>
2020-05-08 09:56:42 +0600

    code:Done

--- main.cpp
+++ main.cpp
@@ -4,8 +4,38 @@
 #include <vector>
 #include <cmath>
 using namespace std;
-const size_t SCREEN_WIDTH = 80;
+const size_t SCREEN_WIDTH = 800;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+
+vector<double>
+input_numbers(size_t count)
+{
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++)
+    {
+        cin >> result[i];
+    }
+    return result;
+}
+
+
+void make_histogram(const vector<double>& numbers, double max, double min, size_t bin_count, vector <size_t>& bins )
+{
+
+    for(double x: numbers)
+    {
+        size_t bin_index=(x-min)/(max-min)*bin_count;
+        if (bin_index==bin_count)
+        {
+            bin_index--;
+        }
+        bins[bin_index]++;
+    }
+}
+
+
+
+
 int main()
 {
     size_t number_count;
@@ -24,10 +54,7 @@ int main()
     cin >> scale;
 
 
-
     vector <size_t> bins(bin_count, 0);
-
-
 
     double min, max;
     find_minmax(numbers, min, max);
