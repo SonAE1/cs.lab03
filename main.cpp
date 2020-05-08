64480ab09fa77bd7aa6eda6ad9ad3ec116910686
Alice (SonAE) <alice@example.com>
2020-05-08 17:34:46 +0600

    code:Сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include "svg.h"
 #include <vector>
 #include <cmath>
+#include <sstream>
+#include <string>
 using namespace std;
 const size_t SCREEN_WIDTH = 800;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
@@ -52,6 +54,7 @@ read_input(istream& in, bool prompt)
     return data;
 }
 
+
 vector<size_t>
 make_histogram( struct Input data)
 {
@@ -72,31 +75,53 @@ make_histogram( struct Input data)
     return bins;
 }
 
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+      const size_t data_size = item_size * item_count;
+      stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+      const char* items2 = reinterpret_cast<const char*>(items);
+      buffer->write(items2, data_size);
+    return data_size ;
+}
 
+Input
+download(const string& address) {
+    stringstream buffer;
 
-int main(int argc, char* argv[])
-{ curl_global_init(CURL_GLOBAL_ALL);
+   curl_global_init(CURL_GLOBAL_ALL);
     CURL* curl = curl_easy_init();
 
-    if (argc>1)
+    if (curl)
     {
             CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            curl_easy_setopt(curl, CURLOPT_URL,address.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
             res = curl_easy_perform(curl);
             if(res!= CURLE_OK)
             {cout<< curl_easy_strerror(res)<< endl;
- exit(1);
-}
+             exit(1);
+            }
             curl_easy_cleanup(curl);
 
-        return 0;
+
     }
 
 
-    Input data = read_input(cin, true);
-    const auto bins = make_histogram (data);
-    show_histogram_svg(bins, data.scale);
-
-
-return 0;
+    return read_input(buffer, false);
 }
+
+
+int main(int argc, char* argv[])
+{  Input input;
+    if (argc > 1) {
+        input = download(argv[1]);
+    } else {
+        input = read_input(cin, true);
+    }
+
+    const auto bins = make_histogram(input);
+    show_histogram_svg(bins, input.scale);
+}
+
+
