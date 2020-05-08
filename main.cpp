2c76822e4c8837689de9c093d61d2b077e800623
Alice (SonAE) <alice@example.com>
2020-05-08 13:39:03 +0600

    code:Read_input(). Передача нужных полей

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ using namespace std;
 const size_t SCREEN_WIDTH = 800;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
 
+struct Input
+{
+    vector<double> numbers;
+    size_t bin_count;
+    size_t scale;
+};
+
 vector<double>
 input_numbers(size_t count)
 {
@@ -19,52 +26,58 @@ input_numbers(size_t count)
 }
 
 
-void make_histogram(const vector<double>& numbers, double max, double min, size_t bin_count, vector <size_t>& bins )
+vector<size_t> make_histogram(istream& in )
 {
-
-    for(double x: numbers)
+    vector<size_t> result(bin_count);
+    double min;
+    double max;
+    find_minmax(data.numbers, min, max);
+    for(double x: data.numbers)
     {
-        size_t bin_index=(x-min)/(max-min)*bin_count;
-        if (bin_index==bin_count)
+        size_t bin_index=(x-min)/(max-min)*data.bin_count;
+        if (bin_index==data.bin_count)
         {
             bin_index--;
         }
-        bins[bin_index]++;
+        result[bin_index]++;
     }
+    return result;
 }
 
 vector<double>
-input_numbers(istream& in, size_t& count)
+input_numbers(istream& in, size_t count)
 {
- cerr << "Enter number count:";
     in >> count;
 }
 
 
+Input
+read_input(istream& in)
+{size_t number_count;
+
+    Input data;
+
+    cerr << "Enter number count: ";
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+
+    cerr << "Enter bin count:";
+    cin >>data.bin_count;
+
+    cerr << "Enter number of scale:";
+    cin >> data.scale;
+
+    return data;
+}
 
 int main()
 {
-    size_t number_count;
 
-    input_numbers(cin, number_count);
-
-    const auto numbers = input_numbers(number_count);
-
-    size_t bin_count;
-    cerr << "Enter bin count:";
-    cin >> bin_count;
-
-    size_t scale;
-    cerr << "Enter number of scale:";
-    cin >> scale;
-
-
-    vector <size_t> bins(bin_count, 0);
-
-    double min, max;
-    find_minmax(numbers, min, max);
-    make_histogram ( numbers, max, min, bin_count, bins );
-    show_histogram_svg(bins, scale);
+    Input data = read_input(cin);
+    const auto bins = make_histogram (data.numbers, data.bin_count) ;
+    show_histogram_svg(bins, data.scale);
     return 0;
 
 }
